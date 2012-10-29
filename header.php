8b22c15142b391a60b630199f66baac2c28fc85c
Ruben de Laat <ruben@logic-labs.nl>
2012-10-29 15:24:24 +0100

    Initial version

---
+++ header.php
@@ -0,0 +1,9 @@
+<?
+	$host = "localhost";
+	$database = "bimservernotifications";
+	$username = "bimser";
+	$password = "[REMOVED]";
+	
+	mysql_connect($host, $username, $password);
+	mysql_select_db($database);
+?>
